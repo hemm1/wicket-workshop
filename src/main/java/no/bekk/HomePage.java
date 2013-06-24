9bb4f7bbd12b90dfa9215a735d66af528a12c8d8
Peter Hemmen <peter.hemmen@gmail.com>
2013-06-24 15:33:52 +0200

    La til e-mail og feedbackpanel

--- src/main/java/no/bekk/HomePage.java
+++ src/main/java/no/bekk/HomePage.java
@@ -8,8 +8,10 @@ import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PropertyListView;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.validation.validator.EmailAddressValidator;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -24,6 +26,8 @@ public class HomePage extends WebPage {
     public HomePage(final PageParameters parameters) {
         super(parameters);
 
+        add(new FeedbackPanel("feedback"));
+
         add(new Label("hello", "Hello World!"));
 
         add(new CommentForm("commentForm"));
@@ -34,6 +38,7 @@ public class HomePage extends WebPage {
             protected void populateItem(ListItem<Comment> item) {
                 item.add(
                         new Label("name"),
+                        new Label("email"),
                         new MultiLineLabel("comment")
                 );
             }
@@ -43,6 +48,8 @@ public class HomePage extends WebPage {
     private class Comment implements Serializable {
         private String name;
 
+        private String email;
+
         private String comment;
     }
 
@@ -50,6 +57,7 @@ public class HomePage extends WebPage {
         public CommentForm(String id) {
             super(id, new CompoundPropertyModel<Comment>(new Comment()));
             add(new TextField<String>("name"));
+            add(new TextField<String>("email").add(EmailAddressValidator.getInstance()));
             add(new TextArea<String>("comment"));
         }
 
@@ -59,6 +67,7 @@ public class HomePage extends WebPage {
             Comment comment = getModelObject();
             comments.add(comment);
             setModelObject(new Comment());
+            info("comment saved successfully!");
         }
     }
 }
